76fd85f423c7424976dc440770c92e1eb496d17b
daniel <danicb740@gmail.com>
2025-11-25 14:02:48 -0500

    Agregar archivos de proyecto.

---
+++ Autopista en fuga 2/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Autopista_en_fuga_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Carro1 = new System.Windows.Forms.PictureBox();
+            this.CarroPrincipal = new System.Windows.Forms.PictureBox();
+            this.Carro2 = new System.Windows.Forms.PictureBox();
+            this.Carro3 = new System.Windows.Forms.PictureBox();
+            this.MovimientoPista = new System.Windows.Forms.Timer(this.components);
+            this.LadoDerecho = new System.Windows.Forms.Timer(this.components);
+            this.LadoIzquierdo = new System.Windows.Forms.Timer(this.components);
+            this.CarroAmarillo = new System.Windows.Forms.Timer(this.components);
+            this.CarroAzulSubido = new System.Windows.Forms.Timer(this.components);
+            this.CarroAzulBajo = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.LadoArriba = new System.Windows.Forms.Timer(this.components);
+            this.LadoAbajo = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carro1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CarroPrincipal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carro2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carro3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(88, -57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 126);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(88, 98);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(16, 126);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox3.Location = new System.Drawing.Point(88, 416);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(16, 126);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox4.Location = new System.Drawing.Point(220, 416);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(16, 126);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox5.Location = new System.Drawing.Point(220, 259);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(16, 126);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox6.Location = new System.Drawing.Point(220, 98);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(16, 126);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox7.Location = new System.Drawing.Point(220, -57);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(16, 126);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox8.Location = new System.Drawing.Point(88, 259);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(16, 126);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(-1, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Puntaje: 0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(242, -1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Velocidad: 0";
+            // 
+            // Carro1
+            // 
+            this.Carro1.Image = global::Autopista_en_fuga_2.Properties.Resources._1_removebg_preview;
+            this.Carro1.Location = new System.Drawing.Point(14, 242);
+            this.Carro1.Name = "Carro1";
+            this.Carro1.Size = new System.Drawing.Size(58, 90);
+            this.Carro1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Carro1.TabIndex = 10;
+            this.Carro1.TabStop = false;
+            // 
+            // CarroPrincipal
+            // 
+            this.CarroPrincipal.Image = global::Autopista_en_fuga_2.Properties.Resources._9_removebg_preview;
+            this.CarroPrincipal.Location = new System.Drawing.Point(128, 359);
+            this.CarroPrincipal.Name = "CarroPrincipal";
+            this.CarroPrincipal.Size = new System.Drawing.Size(58, 90);
+            this.CarroPrincipal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CarroPrincipal.TabIndex = 11;
+            this.CarroPrincipal.TabStop = false;
+            // 
+            // Carro2
+            // 
+            this.Carro2.Image = global::Autopista_en_fuga_2.Properties.Resources._7_removebg_preview;
+            this.Carro2.Location = new System.Drawing.Point(128, 34);
+            this.Carro2.Name = "Carro2";
+            this.Carro2.Size = new System.Drawing.Size(62, 90);
+            this.Carro2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Carro2.TabIndex = 12;
+            this.Carro2.TabStop = false;
+            // 
+            // Carro3
+            // 
+            this.Carro3.Image = global::Autopista_en_fuga_2.Properties.Resources._8_removebg_preview;
+            this.Carro3.Location = new System.Drawing.Point(264, 134);
+            this.Carro3.Name = "Carro3";
+            this.Carro3.Size = new System.Drawing.Size(58, 90);
+            this.Carro3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Carro3.TabIndex = 13;
+            this.Carro3.TabStop = false;
+            // 
+            // MovimientoPista
+            // 
+            this.MovimientoPista.Enabled = true;
+            this.MovimientoPista.Interval = 10;
+            this.MovimientoPista.Tick += new System.EventHandler(this.MovimientoPista_Tick);
+            // 
+            // LadoDerecho
+            // 
+            this.LadoDerecho.Interval = 10;
+            this.LadoDerecho.Tick += new System.EventHandler(this.LadoDerecho_Tick);
+            // 
+            // LadoIzquierdo
+            // 
+            this.LadoIzquierdo.Interval = 10;
+            this.LadoIzquierdo.Tick += new System.EventHandler(this.LadoIzquierdo_Tick);
+            // 
+            // CarroAmarillo
+            // 
+            this.CarroAmarillo.Enabled = true;
+            this.CarroAmarillo.Interval = 10;
+            this.CarroAmarillo.Tick += new System.EventHandler(this.CarroAmarillo_Tick);
+            // 
+            // CarroAzulSubido
+            // 
+            this.CarroAzulSubido.Enabled = true;
+            this.CarroAzulSubido.Interval = 10;
+            this.CarroAzulSubido.Tick += new System.EventHandler(this.CarroAzulSubido_Tick);
+            // 
+            // CarroAzulBajo
+            // 
+            this.CarroAzulBajo.Enabled = true;
+            this.CarroAzulBajo.Interval = 10;
+            this.CarroAzulBajo.Tick += new System.EventHandler(this.CarroAzulBajo_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Font = new System.Drawing.Font("Agency FB", 26F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Firebrick;
+            this.label3.Location = new System.Drawing.Point(68, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 42);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "FIN DEL JUEGO";
+            this.label3.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RosyBrown;
+            this.button1.Font = new System.Drawing.Font("Agency FB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(110, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 53);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Repetir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LadoArriba
+            // 
+            this.LadoArriba.Interval = 10;
+            this.LadoArriba.Tick += new System.EventHandler(this.LadoArriba_Tick_1);
+            // 
+            // LadoAbajo
+            // 
+            this.LadoAbajo.Interval = 10;
+            this.LadoAbajo.Tick += new System.EventHandler(this.LadoAbajo_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(334, 461);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Carro3);
+            this.Controls.Add(this.Carro2);
+            this.Controls.Add(this.CarroPrincipal);
+            this.Controls.Add(this.Carro1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(350, 500);
+            this.MinimumSize = new System.Drawing.Size(350, 500);
+            this.Name = "Form1";
+            this.Text = "Autopista en fuga 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carro1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CarroPrincipal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carro2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Carro3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox Carro1;
+        private System.Windows.Forms.PictureBox CarroPrincipal;
+        private System.Windows.Forms.PictureBox Carro2;
+        private System.Windows.Forms.PictureBox Carro3;
+        private System.Windows.Forms.Timer MovimientoPista;
+        private System.Windows.Forms.Timer LadoDerecho;
+        private System.Windows.Forms.Timer LadoIzquierdo;
+        private System.Windows.Forms.Timer CarroAmarillo;
+        private System.Windows.Forms.Timer CarroAzulSubido;
+        private System.Windows.Forms.Timer CarroAzulBajo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer LadoArriba;
+        private System.Windows.Forms.Timer LadoAbajo;
+    }
+}
+
---
+++ Autopista en fuga 2/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autopista_en_fuga_2
+{
+    public partial class Form1 : Form
+    {
+        private int Velocidad;
+        private PictureBox[] road = new PictureBox[8];
+        private int Puntaje = 0;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Velocidad = 3;
+            road[0] = pictureBox1;
+            road[1] = pictureBox2;
+            road[2] = pictureBox3;
+            road[3] = pictureBox4;
+            road[4] = pictureBox5;
+            road[5] = pictureBox6;
+            road[6] = pictureBox7;
+            road[7] = pictureBox8;
+
+        }
+
+        private void MovimientoPista_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox pb in road)
+            {
+                pb.Top += Velocidad;
+                if (pb.Top >= this.Height)
+                {
+                    pb.Top = -pb.Height;
+                }
+            }
+            if (Puntaje > 10 && Puntaje < 30)
+            {
+                Velocidad = 5;
+            }
+            if (Puntaje > 30 && Puntaje < 50)
+            {
+                Velocidad = 6;
+            }
+            if (Puntaje > 50 && Puntaje < 70)
+            {
+                Velocidad = 7;
+            }
+            if (Puntaje > 100)
+            {
+                Velocidad = 9;
+            }
+            label2.Text = "Velocidad" + Velocidad;
+            if ((CarroPrincipal.Bounds.IntersectsWith(Carro1.Bounds)))
+            {
+                endgame();
+            }
+            if ((CarroPrincipal.Bounds.IntersectsWith(Carro2.Bounds)))
+            {
+                endgame();
+            }
+            if ((CarroPrincipal.Bounds.IntersectsWith(Carro3.Bounds)))
+            {
+                endgame();
+            }
+
+        }
+        private void endgame()
+        {
+            button1.Visible = true;
+            label3.Visible = true;
+            MovimientoPista.Stop();
+            CarroAmarillo.Stop();
+            CarroAzulSubido.Stop();
+            CarroAzulBajo.Stop();
+        }
+
+        private void LadoDerecho_Tick(object sender, EventArgs e)
+        {
+            if (CarroPrincipal.Location.X < 275)
+            {
+                CarroPrincipal.Left += 5;
+            }
+        }
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                LadoDerecho.Start();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                LadoIzquierdo.Start();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                LadoArriba.Start();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                LadoAbajo.Start();
+            }
+        }
+
+        
+
+        private void LadoIzquierdo_Tick(object sender, EventArgs e)
+        {
+            if (CarroPrincipal.Location.X > 0)
+            {
+                CarroPrincipal.Left -= 5;
+            }
+        }
+        
+       
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            LadoDerecho.Stop();
+            LadoIzquierdo.Stop();
+            LadoArriba.Stop();
+            LadoAbajo.Stop();
+        }
+
+        private void CarroAmarillo_Tick(object sender, EventArgs e)
+        {
+            Carro1.Top += Velocidad / 2;
+            if (Carro1.Top >= this.Height)
+            {
+                Puntaje += 1;
+                label1.Text = "Puntaje " + Puntaje;
+
+
+
+                Random rnd = new Random();
+                Carro1.Top = -(Convert.ToInt32(Math.Ceiling(rnd.NextDouble() * 200)) + Carro1.Height);
+                Carro1.Left = Convert.ToInt32(Math.Ceiling(rnd.NextDouble() * 50)) + 0;
+            }
+        }
+
+
+        private void CarroAzulSubido_Tick(object sender, EventArgs e)
+        {
+            Carro2.Top += Velocidad / 3;
+            if (Carro2.Top >= this.Height)
+            {
+                Puntaje += 1;
+                label1.Text = "Puntaje " + Puntaje;
+
+                Random rnd = new Random();
+                Carro2.Top = -(Convert.ToInt32(Math.Ceiling(rnd.NextDouble() * 200)) + Carro2.Height);
+                Carro2.Left = Convert.ToInt32(Math.Ceiling(rnd.NextDouble() * 50)) + 100;
+            }
+        }
+        private void CarroAzulBajo_Tick(object sender, EventArgs e)
+        {
+            Carro3.Top += Velocidad * 1 / 2;
+            if (Carro3.Top >= this.Height)
+            {
+                Puntaje += 1;
+                label1.Text = "Puntaje " + Puntaje;
+                Random rnd = new Random();
+                Carro3.Top = -(Convert.ToInt32(Math.Ceiling(rnd.NextDouble() * 200)) + Carro3.Height);
+                Carro3.Left = Convert.ToInt32(Math.Ceiling(rnd.NextDouble() * 120)) + 180;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Puntaje = 0;
+            this.Controls.Clear();
+            InitializeComponent();
+            Form1_Load(e, e);
+        }
+
+        private void LadoArriba_Tick_1(object sender, EventArgs e)
+        {
+            if (CarroPrincipal.Top > 0) 
+            {
+                CarroPrincipal.Top -= 5;
+            }
+        }
+
+        private void LadoAbajo_Tick_1(object sender, EventArgs e)
+        {
+            if (CarroPrincipal.Bottom < this.ClientSize.Height)
+            {
+                CarroPrincipal.Top += 5;
+            }
+        }
+    }
+}
